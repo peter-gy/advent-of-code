84384557d5c5ca95df622f7bbd05e6f9464087d3
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-12-01 07:57:34 +0100

    day-1 : n-sum using itertools

--- 2020/01/main.py
+++ 2020/01/main.py
@@ -1,2 +1,21 @@
+def n_sum(numbers, target_sum, n):
+    def tuple_has_target_sum(t):
+        return sum(t) == target_sum
+
+    from itertools import combinations
+    return list(filter(tuple_has_target_sum, list(combinations(numbers, n))))
+
+
+def solve(numbers, target_sum, n):
+    tuples = n_sum(numbers, target_sum, n)
+    if not tuples:
+        raise RuntimeError(f'No {n}-tuple exists which has a sum of {target_sum}')
+    from math import prod
+    product = prod(tuples[0])
+    print(f'n = {n}, product = {product}')
+
+
 if __name__ == '__main__':
-    print('Hi')
+    numbers = [int(line) for line in open('input.txt', 'r').readlines()]
+    solve(numbers, 2020, 2)
+    solve(numbers, 2020, 3)
